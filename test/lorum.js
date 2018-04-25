b74a378d9acc9fab1a58cc6bfd2cd480bbce9838
Jan Badenhorst <janhendrik.badenhorst@gmail.com>
2018-04-26 00:54:47 +0200

    Tests with content

---
+++ test/lorum.js
@@ -0,0 +1,22 @@
+
+const lorum = `Lorem ipsum dolor sit amet, eu nec tantas soleat lucilius. At mea graeco aliquid, mei ex perfecto intellegebat, ius possim everti id. At ponderum mediocrem sadipscing eos, nonumy aperiri evertitur vel ne. Eum zril ornatus ei, vivendum tractatos ex cum, nam temporibus definitiones ei. Vis vide iuvaret epicurei in, eius consul theophrastus ut nam, erant accumsan est eu. Rebum quaestio maluisset at mea, ei usu melius vituperata.
+
+Ei pri possim vocent, ad quo nibh facete. Mea ex graeco quaeque honestatis, in persius nostrud salutatus mei. Ne eos habeo malorum concludaturque, has et inermis voluptaria. Legere intellegat argumentum est ne, ne nostro debitis vim. Erant dicant signiferumque in sit, cu dicit ullamcorper duo.
+
+Et probatus facilisi mel. Utamur qualisque te eam, modo sale vidit vis et. Facete legendos vel ne. Cu pri integre feugiat, no stet illud mei, pri id quod delicatissimi. Eum veniam hendrerit te.
+
+Ex eum erroribus scriptorem, pri conceptam vituperata ea. Est eruditi civibus constituto an, mei bonorum deleniti et, mei cu vocent veritus scribentur. Ceteros patrioque scribentur no est, doming fierent quaestio quo at. Eum delenit necessitatibus ad, ea nec solet commodo dignissim. Usu no ludus accumsan pertinacia, mea regione praesent delicatissimi ne. Cu possim cetero fabulas pri, porro tincidunt has at.
+
+Eu velit sadipscing eam. Democritum reformidans quo no. Has scriptorem efficiantur an. Pro fugit dolore persius ne, habeo iisque sensibus eam ad, sanctus definitiones ei sed.
+
+Ad offendit salutandi prodesset pro, nibh simul disputando id qui. Nam cu quas veniam constituam, eu everti utamur cotidieque sea. Essent saperet omittantur in usu. An per latine reformidans definitionem. Appetere mandamus cu vel, ferri sensibus quo no. An paulo choro officiis sit, modo detraxit deseruisse pri ad.
+
+Pro deserunt expetenda at, an sea gloriatur dissentiet, clita labores no eos. Has agam conceptam eu, quando regione nam at. Mei ex dolor veritus consequuntur, mea nobis latine placerat cu, nec dolore mollis in. Ad pri epicurei abhorreant mediocritatem, ius ut graece latine. Falli legimus consequuntur eum ei. Qui at quando maiorum expetenda, pri ea errem eruditi corpora.
+
+Et quem appellantur eum, id sit eruditi docendi definiebas, ut eam nulla deseruisse. At est putant perfecto qualisque, dicam incorrupte scripserit id sit, indoctum mediocrem honestatis cum an. Graecis appellantur cum ad, mel in iudico electram expetenda, natum cetero comprehensam eam an. Utroque definiebas disputando qui an, torquatos adversarium eos id. Sed dico meis lucilius at, nec cibo dicta percipitur ad. At erant legimus his, id mea diceret sententiae, in pro viris audiam erroribus.
+
+Vidit decore ea pri, sea congue lucilius te. Porro splendide ei eum, eu quo scaevola aliquando intellegam. Graece partiendo moderatius mei no. Elit postulant vim ea, cum ne adversarium intellegebat. Eam et mundi appellantur, dolor admodum vel id. Vis an oblique labitur fabellas, mel an lucilius vulputate.
+
+Cum ad elitr congue salutandi. An inani argumentum scribentur sea, cum ne fabellas democritum complectitur. Nec ut quod conclusionemque, nam esse falli graecis ad. Est unum quaerendum eloquentiam at, sit ei elit omnes accommodare, ancillae suscipit philosophia qui no. Rebum efficiantur at eum, ad sit stet mandamus. No eum iuvaret gloriatur comprehensam, ius ne quod ignota nostrum.`;
+
+export default lorum;
