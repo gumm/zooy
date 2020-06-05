89674900c5fa82d5fff6884b4b32f2136cdc028f
Jan Badenhorst <jan@trintel.co.za>
2020-06-05 09:10:17 +0200

    Don't try and read the dataset if no element exists.

--- main.js
+++ main.js
@@ -827,7 +827,7 @@ const toggleClass = (element, className) => {
 };
 
 
-const getElDataMap = el => Object.assign({}, el.dataset || {});
+const getElDataMap = el => el ? Object.assign({}, el.dataset || {}) : {};
 
 
 const dtFormatter = {
