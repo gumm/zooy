e928d0897d4f029c008f93925aad1b2e00ea3b61
Jan Badenhorst <jan@trintel.co.za>
2022-04-16 10:48:37 +0200

    Implement abort controller

--- src/main.js
+++ src/main.js
@@ -4,12 +4,11 @@ import Panel from './ui/panel.js';
 import FormPanel from './ui/form.js';
 import Split from './ui/split.js';
 import UserManager from './user/usermanager.js';
-import View from './ui/view.js';
-import { UiEventType } from './events/uieventtype.js';
-import { treeNodeSelect} from "./ui/view.js";
+import View, {treeNodeSelect} from './ui/view.js';
+import {UiEventType} from './events/uieventtype.js';
 import Conductor from './ui/conductor.js';
-import * as domUtils from './dom/utils.js'
-import * as uriUtils from './uri/uri.js'
+import * as domUtils from './dom/utils.js';
+import * as uriUtils from './uri/uri.js';
 
 const zooy = {
   Component,
